fb5f69ecf25b9fd1236e60777ec373b5ddaa82ed
izmi.komar <komar.izmi@gmail.com>
2023-11-11 19:47:53 +0700

    remove import lazy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
+import About from "@/view/About";
+import Home from "@/view/Home";
+import NotFound from "@/view/404";
+import Profile from "@/view/Profile";
 import { createBrowserRouter } from "react-router-dom";
-import { lazy } from "react";
 
-const Home = lazy(() => import("@/view/Home"));
-const NotFound = lazy(() => import("@/view/404"));
-const About = lazy(() => import("@/view/About"));
-const Profile = lazy(() => import("@/view/Profile"));
 export const routes = createBrowserRouter([
   {
     path: "/",
