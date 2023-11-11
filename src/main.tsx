fce2db4acd2b8d37efb3fd5dae1f77a74b95b685
izmi.komar <komar.izmi@gmail.com>
2023-11-11 19:49:34 +0700

    remove suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import React, { Suspense } from "react";
+import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -8,8 +8,6 @@ import { routes } from "./routes/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Suspense fallback={<div>loading...</div>}>
-      <RouterProvider router={routes} />
-    </Suspense>
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
