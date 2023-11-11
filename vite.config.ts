a4c84326a5d1efad7426e5fc67b2f4e6ffeecbd6
izmi.komar <komar.izmi@gmail.com>
2023-11-11 19:57:48 +0700

    set mode production

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import path from "path";
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-    }
-  }
-})
+    },
+  },
+  mode: "production",
+});
